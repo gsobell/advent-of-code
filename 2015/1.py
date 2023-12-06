eccdccc73c7d48a3e3c321f181262b975af4b2c0
gabriel <gsobell@users.noreply.github.com>
2023-12-06 16:20:43 +0200

    Day 1, 2015

---
+++ 2015/1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from sys import setrecursionlimit
+
+with open ("1.in") as f:
+     a = f.read()
+
+setrecursionlimit(len(a) * 2 ) 
+
+def counter(s: str, count : int = 0) -> int:
+    if len(s) == 0:
+        return count
+    elif s[0] == '(':
+        count += 1
+    elif s[0] == ')':
+        count -= 1
+    return counter(s[1:], count)
+
+
+def basement(s: str, count : int = 0, step : int = 0) -> int:
+    if count == -1:
+        return step
+    else:
+        step += 1
+    if s[0] == '(':
+        count += 1
+    elif s[0] == ')':
+        count -= 1
+    return basement(s[1:], count, step)
+
+print(counter(a))
+print(basement(a))
---
+++ 2015/README.md
@@ -0,0 +1,3 @@
+# advent of code 2015
+
+Advent of code solutions to 2015 in functional python
